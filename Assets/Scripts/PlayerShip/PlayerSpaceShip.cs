f7eff9025e9e46c0856a74daace8fe4fdfbd6b7e
Nicholas Rawitscher <nicholas@bimorph.com>
2021-07-19 18:32:35 -0500

    init commit

---
+++ Assets/Scripts/PlayerShip/PlayerSpaceShip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.PlayerShip
+{
+    public class PlayerSpaceShip : MonoBehaviour
+    {
+        private Transform _transform;
+        void Start()
+        {
+            _transform = this.transform;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerShip/PlayerSpaceShipMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Interfaces;
+using UnityEngine;
+
+public class PlayerSpaceShipMove : IMove
+{
+    private readonly Transform _transform;
+
+    public double MaxSpeed { get; set; }
+
+    internal PlayerSpaceShipMove(Transform transform, double maxSpeed)
+    {
+        _transform = transform;
+
+        this.MaxSpeed = maxSpeed;
+    }
+
+    public void Move()
+    {
+        throw new System.NotImplementedException();
+    }
+}
